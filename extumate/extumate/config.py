5db43a551d3db7bd0bb710aaabd732a04996178a
anne717 <18551702+anne717@users.noreply.github.com>
2020-09-27 14:56:23 -0400

    First commit: extumate package

---
+++ extumate/extumate/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 10:00:15 2020
+
+@author: anne_717
+"""
+from pathlib import Path  # pathlib is seriously awesome!
+
+data_dir = '../data/feathered/'
+#data_path = data_dir / 'my_file.csv'  # use feather files if possible!!!
+
+#customer_db_url = 'sql:///customer/db/url'
+#purchases_db_url = 'sql:///purchases/db/url'
+
+extumate_engine_url = 'postgresql://postgres:password@localhost:5432/extumate'
---
+++ extumate/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 10:46:53 2020
+
+@author: anne_717
+"""
+
+from setuptools import setup, find_packages
+
+setup(name="extumate",
+      version="0.1")
